d5a3bfe464d9ad0c1ec3bdce7e2eb68ac160894e
Jakub Piotrowski <jakub.piotrowski@lekta.ai>
2018-04-22 22:42:47 +0200

    Seed update

--- src/Infrastructure/Data/CatalogContextSeed.cs
+++ src/Infrastructure/Data/CatalogContextSeed.cs
@@ -59,11 +59,9 @@ namespace Infrastructure.Data
         {
             return new List<CatalogBrand>()
             {
-                new CatalogBrand() { Brand = "Azure"},
-                new CatalogBrand() { Brand = ".NET" },
-                new CatalogBrand() { Brand = "Visual Studio" },
-                new CatalogBrand() { Brand = "SQL Server" }, 
-                new CatalogBrand() { Brand = "Other" }
+                new CatalogBrand() { Brand = "Nike"},
+				new CatalogBrand() { Brand = "Adidas" },
+                new CatalogBrand() { Brand = "Puma" }
             };
         }
 
@@ -72,9 +70,7 @@ namespace Infrastructure.Data
             return new List<CatalogType>()
             {
                 new CatalogType() { Type = "Mug"},
-                new CatalogType() { Type = "T-Shirt" },
-                new CatalogType() { Type = "Sheet" },
-                new CatalogType() { Type = "USB Memory Stick" }
+                new CatalogType() { Type = "T-Shirt" }
             };
         }
 
@@ -82,18 +78,18 @@ namespace Infrastructure.Data
         {
             return new List<CatalogItem>()
             {
-                new CatalogItem() { CatalogTypeId=2,CatalogBrandId=2, Description = ".NET Bot Black Sweatshirt", Name = ".NET Bot Black Sweatshirt", Price = 19.5M, PictureUri = "http://catalogbaseurltobereplaced/images/products/1.png" },
-                new CatalogItem() { CatalogTypeId=1,CatalogBrandId=2, Description = ".NET Black & White Mug", Name = ".NET Black & White Mug", Price= 8.50M, PictureUri = "http://catalogbaseurltobereplaced/images/products/2.png" },
-                new CatalogItem() { CatalogTypeId=2,CatalogBrandId=5, Description = "Prism White T-Shirt", Name = "Prism White T-Shirt", Price = 12, PictureUri = "http://catalogbaseurltobereplaced/images/products/3.png" },
-                new CatalogItem() { CatalogTypeId=2,CatalogBrandId=2, Description = ".NET Foundation Sweatshirt", Name = ".NET Foundation Sweatshirt", Price = 12, PictureUri = "http://catalogbaseurltobereplaced/images/products/4.png" },
-                new CatalogItem() { CatalogTypeId=3,CatalogBrandId=5, Description = "Roslyn Red Sheet", Name = "Roslyn Red Sheet", Price = 8.5M, PictureUri = "http://catalogbaseurltobereplaced/images/products/5.png" },
-                new CatalogItem() { CatalogTypeId=2,CatalogBrandId=2, Description = ".NET Blue Sweatshirt", Name = ".NET Blue Sweatshirt", Price = 12, PictureUri = "http://catalogbaseurltobereplaced/images/products/6.png" },
-                new CatalogItem() { CatalogTypeId=2,CatalogBrandId=5, Description = "Roslyn Red T-Shirt", Name = "Roslyn Red T-Shirt", Price = 12, PictureUri = "http://catalogbaseurltobereplaced/images/products/7.png"  },
-                new CatalogItem() { CatalogTypeId=2,CatalogBrandId=5, Description = "Kudu Purple Sweatshirt", Name = "Kudu Purple Sweatshirt", Price = 8.5M, PictureUri = "http://catalogbaseurltobereplaced/images/products/8.png" },
-                new CatalogItem() { CatalogTypeId=1,CatalogBrandId=5, Description = "Cup<T> White Mug", Name = "Cup<T> White Mug", Price = 12, PictureUri = "http://catalogbaseurltobereplaced/images/products/9.png" },
-                new CatalogItem() { CatalogTypeId=3,CatalogBrandId=2, Description = ".NET Foundation Sheet", Name = ".NET Foundation Sheet", Price = 12, PictureUri = "http://catalogbaseurltobereplaced/images/products/10.png" },
-                new CatalogItem() { CatalogTypeId=3,CatalogBrandId=2, Description = "Cup<T> Sheet", Name = "Cup<T> Sheet", Price = 8.5M, PictureUri = "http://catalogbaseurltobereplaced/images/products/11.png" },
-                new CatalogItem() { CatalogTypeId=2,CatalogBrandId=5, Description = "Prism White TShirt", Name = "Prism White TShirt", Price = 12, PictureUri = "http://catalogbaseurltobereplaced/images/products/12.png" }
+				new CatalogItem() { CatalogTypeId=2,CatalogBrandId=2, Description = "Nike Bot Black Sweatshirt", Name = "Nike Bot Black Sweatshirt", Price = 19.5M, PictureUri = "http://catalogbaseurltobereplaced/images/products/1.png" },
+				new CatalogItem() { CatalogTypeId=1,CatalogBrandId=2, Description = "Adidas Black & White Mug", Name = "Adidas Black & White Mug", Price= 8.50M, PictureUri = "http://catalogbaseurltobereplaced/images/products/2.png" },
+				new CatalogItem() { CatalogTypeId=2,CatalogBrandId=5, Description = "Puma White T-Shirt", Name = "Puma White T-Shirt", Price = 12, PictureUri = "http://catalogbaseurltobereplaced/images/products/3.png" },
+				new CatalogItem() { CatalogTypeId=2,CatalogBrandId=2, Description = "Nike Foundation Sweatshirt", Name = "Nike Foundation Sweatshirt", Price = 12, PictureUri = "http://catalogbaseurltobereplaced/images/products/4.png" },
+				new CatalogItem() { CatalogTypeId=3,CatalogBrandId=5, Description = "Adidas Red Sheet", Name = "Adidas Red Sheet", Price = 8.5M, PictureUri = "http://catalogbaseurltobereplaced/images/products/5.png" },
+				new CatalogItem() { CatalogTypeId=2,CatalogBrandId=2, Description = "Puma Blue Sweatshirt", Name = "Puma Blue Sweatshirt", Price = 12, PictureUri = "http://catalogbaseurltobereplaced/images/products/6.png" },
+				new CatalogItem() { CatalogTypeId=2,CatalogBrandId=5, Description = "Nike Red T-Shirt", Name = "Nike Red T-Shirt", Price = 12, PictureUri = "http://catalogbaseurltobereplaced/images/products/7.png"  },
+				new CatalogItem() { CatalogTypeId=2,CatalogBrandId=5, Description = "Adidas Purple Sweatshirt", Name = "Adidas Purple Sweatshirt", Price = 8.5M, PictureUri = "http://catalogbaseurltobereplaced/images/products/8.png" },
+				new CatalogItem() { CatalogTypeId=1,CatalogBrandId=5, Description = "Puma White Mug", Name = "Puma White Mug", Price = 12, PictureUri = "http://catalogbaseurltobereplaced/images/products/9.png" },
+				new CatalogItem() { CatalogTypeId=3,CatalogBrandId=2, Description = "Nike Foundation Sheet", Name = "Nike Foundation Sheet", Price = 12, PictureUri = "http://catalogbaseurltobereplaced/images/products/10.png" },
+				new CatalogItem() { CatalogTypeId=3,CatalogBrandId=2, Description = "Adidas Sheet", Name = "Adidas Sheet", Price = 8.5M, PictureUri = "http://catalogbaseurltobereplaced/images/products/11.png" },
+				new CatalogItem() { CatalogTypeId=2,CatalogBrandId=5, Description = "Puma White TShirt", Name = "Puma White TShirt", Price = 12, PictureUri = "http://catalogbaseurltobereplaced/images/products/12.png" }
             };
         }
     }
